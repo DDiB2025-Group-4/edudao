f12520c47127bc8e73c9a9f931a2112f237ad768
inaridiy <inainainariinainari@gmail.com>
2025-07-14 13:47:31 +0200

    feat:

--- packages/frontend/src/routes/__root.tsx
+++ packages/frontend/src/routes/__root.tsx
@@ -1,14 +1,11 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
-import Header from '../components/Header'
-
-import TanStackQueryLayout from '../integrations/tanstack-query/layout.tsx'
-
-import type { QueryClient } from '@tanstack/react-query'
+import type { QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Header from "../components/Header";
+import TanStackQueryLayout from "../integrations/tanstack-query/layout.tsx";
 
 interface MyRouterContext {
-  queryClient: QueryClient
+  queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
@@ -22,4 +19,4 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       <TanStackQueryLayout />
     </>
   ),
-})
+});
