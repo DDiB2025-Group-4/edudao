e8a86665367c1c24ac36f62ae68714b85b77f2f5
inaridiy <inainainariinainari@gmail.com>
2025-07-16 01:33:39 +0200

    feat: selective discroger

--- packages/frontend/src/routes/holder.$tokenId.tsx
+++ packages/frontend/src/routes/holder.$tokenId.tsx
@@ -70,7 +70,7 @@ function RouteComponent() {
 
     const payloadToSign = {
       studentAddress: credential.claims.address,
-      presantation: await sdjwt.present(credential.sdjwt),
+      presantation: await sdjwt.present(credential.sdjwt, selectiveDisclosure),
       timestamp: Date.now(),
     };
     const message = JSON.stringify(payloadToSign);
